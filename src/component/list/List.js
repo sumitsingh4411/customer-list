cd43fe3567c685b1b58a9bd5b97dee9a139731b0
sumitsingh4411 <shiv6371913458@gmail.com>
2021-06-21 23:35:05 +0530

    adding  bid option

--- src/component/list/List.js
+++ src/component/list/List.js
@@ -17,50 +17,64 @@ const useStyles = makeStyles({
 });
 
 const List = ({ posts, loading }) => {
-  const [max,setmax]=useState(true);
-  const [bid, setbids]=useState([]);
+  const [max, setmax] = useState(true);
+  const [bid, setbids] = useState([]);
+  const [sbid, ssetbids] = useState(1000000);
+  const [mbid, msetbids] = useState(-1000000);
   const classes = useStyles();
   if (loading) {
     return <h2>Loading...</h2>;
   }
-  
- 
+
+
   return (
     <TableContainer component={Paper}>
-    <Table className={classes.table} aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Customer name</TableCell>
-          <TableCell align="right"> Email</TableCell>
-          <TableCell align="right"> Phone &nbsp;(g)</TableCell>
-          <TableCell align="right"> Premium &nbsp;(g)</TableCell>
-          <TableCell align="right">Max/Min bid&nbsp;(g)</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {posts.map((row) => (
-          <TableRow key={row.id}>
-            <TableCell component="th" scope="row">
-              <div style={{display:'flex'}}>
-              {row.firstname +"  "+ row.lastname}
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <Avatar alt={row.firstname} src={row.avatarUrl}/>
-              </div>
-            </TableCell>
-            <TableCell align="right">{row.email}</TableCell>
-            <TableCell align="right">{row.phone}</TableCell>
-            <TableCell align="right">{row.hasPremium===true? "true":"false"}</TableCell>
-            <TableCell align="right">
-             <div style={{display:'flex'}} >
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <BootstrapSwitchButton checked={true}>max</BootstrapSwitchButton>
-             </div>
-            </TableCell>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Customer name</TableCell>
+            <TableCell align="right"> Email</TableCell>
+            <TableCell align="right"> Phone &nbsp;(g)</TableCell>
+            <TableCell align="right"> Premium &nbsp;(g)</TableCell>
+            <TableCell align="right">Max/Min bid&nbsp;(g)</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+        </TableHead>
+        <TableBody>
+          {posts.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                <div style={{ display: 'flex' }}>
+                  {row.firstname + "  " + row.lastname}
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <Avatar alt={row.firstname} src={row.avatarUrl} />
+                </div>
+              </TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.phone}</TableCell>
+              <TableCell align="right">{row.hasPremium === true ? "true" : "false"}</TableCell>
+              <TableCell align="right">
+                <div style={{ display: 'flex' }} >
+                  {
+                    row.bids.map(e=>{
+                       if(e.amount>mbid)
+                         msetbids(e.amount);
+                       if(e.amout<sbid)
+                         ssetbids(e.amount);
+                          
+                    })
+                  }
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  {
+                     mbid!==1000000 && sbid!==-1000000 &&  max===true  ? sbid : mbid
+                  }
+                  <BootstrapSwitchButton checked={true} onChane={()=>{setmax(false)}}>max</BootstrapSwitchButton>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
